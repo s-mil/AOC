c8c453decfda0bfbab086f3e59800e8a8afb83ee
Sam Miller <23262411+s-mil@users.noreply.github.com>
2024-09-07 17:33:55 -0500

    change to nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Adevent of Code devshell for rs, c#, python, & go";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in
+  {
+    # importing package example
+    # packages."x86_64-linux".default = 
+    #   pkgs.callPackage (import ./default.nix) {};
+
+    devShells."x86_64-linux".default = pkgs.mkShell {
+
+      packages = [ pkgs.go pkgs.python312 pkgs.dotnetCorePackages.aspnetcore_8_0_3xx ];
+
+      inputsFrom = [ pkgs.bat ];
+      
+    };
+  };
+}
+
